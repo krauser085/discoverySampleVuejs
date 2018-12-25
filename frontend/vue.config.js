ece9d2c119a66a9a39bc612a1279e37a3ec56463
guitarYD <krauser085@gmail.com>
2018-12-25 18:59:12 +0900

    set proxy for dev env

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   outputDir: '../backend/dist',
   devServer: {
     proxy: {
-      '/discovery': { target: 'http://localhost:3000/discovery' }
+      '^/documents': { target: 'http://localhost:3000/' }
     }
   },
   configureWebpack: {
