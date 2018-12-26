74c9699f75702fd8de49197b727079a37e57d4f6
guitarYD <krauser085@gmail.com>
2018-12-26 23:28:19 +0900

    bind parent property with child component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,21 +1,25 @@
 <template>
- <div>
-   <header>
-     <h2 class="container">Search Documents</h2>
-   </header>
-   <div class="container">
-     <search-form>
-
-     </search-form>
-   </div>
- </div>
+  <div>
+    <header>
+      <h2 class="container">Search Documents</h2>
+    </header>
+    <div class="container">
+      <search-form :query="query"></search-form>
+    </div>
+  </div>
 </template>
 
 <script>
 import FormComponent from './components/FormComponent.vue'
 export default {
+  name: 'app',
   components: {
     'search-form': FormComponent
+  },
+  data () {
+    return {
+      query: 'this is test'
+    }
   }
 }
 </script>
--- frontend/src/components/FormComponent.vue
+++ frontend/src/components/FormComponent.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    <input type="text">
+    <input type="text" v-model="value">
   </div>
 </template>
 
 <script>
 export default {
-
+  props: ['query'],
+  data () {
+    return {
+      value: this.query
+    }
+  },
+  watch: {
+    query (newV, oldV) {
+      this.value = newV
+    }
+  }
 }
 </script>
 
-<style lang="sass">
+<style lang='sass'>
 
 </style>
